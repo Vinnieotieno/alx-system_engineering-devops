4a82e7d22ceb48d43391a6ff5fb00833a8d587e2
Vinnieotieno <vinnieotieno004@gmail.com>
2025-06-29 01:25:21 +0300

    restructured the scripts

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,9 @@
-file_line { 'Turn off passwd auth':
+file_line { 'Turn off password auth':
   path  => '/etc/ssh/ssh_config',
   line  => '    PasswordAuthentication no',
-  match => '^.*PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => '    IdentityFile ~/.ssh/school',
-  match => '^.*IdentityFile',
 }
